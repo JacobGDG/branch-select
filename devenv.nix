8aefe423d80640913faca89a14730a047b74abd6
JacobGDG <10035081+JacobGDG@users.noreply.github.com>
2025-01-06 21:59:22 +0000

    Initial Commit - devenv

---
+++ devenv.nix
@@ -0,0 +1,25 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/languages/
+  languages.rust.enable = true;
+
+  enterShell = ''
+    zsh
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+}
